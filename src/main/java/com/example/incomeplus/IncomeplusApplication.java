a1337af742f11baeb9a1d94b14719744b4fc4062
ys <ys.heo.korea@gmail.com>
2022-02-18 22:47:32 +0900

    Initial commit

---
+++ src/main/java/com/example/incomeplus/IncomeplusApplication.java
@@ -0,0 +1,15 @@
+package com.example.incomeplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncomeplusApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(IncomeplusApplication.class, args);
+		System.out.println("hello springBoot!");
+	}
+
+}
---
+++ src/test/java/com/example/incomeplus/IncomeplusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.incomeplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IncomeplusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
